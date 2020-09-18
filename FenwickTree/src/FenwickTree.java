83260bff4b78f1bc7d2cae479c553c6b1f76624f
Anit George <anitgeorge89@gmail.com>
2020-09-18 22:35:02 +0530

    Fenwick Tree

--- FenwickTree/src/FenwickTree.java
+++ FenwickTree/src/FenwickTree.java
@@ -1,45 +1,50 @@
-/**
- *
- * @author anitgeorge
- */
+class FemwickTree {
 
-public class FenwickTree {
+    int[] nums;
+    int[] BIT;
+    int n;
 
-    int N;
-    long[] tree;
-    FenwickTree(long[] nums){
-        N = nums.length;
-        tree = new long[N + 1];
-        tree[0] = 0L;
-        tree = nums.clone();
-        for(int i = 1; i < N; i++){
-            int parent = i + lsb(i);
-            if(parent < N)
-                tree[parent] += tree[i];
+    public FemwickTree(int[] nums) {
+        this.nums = nums;
+
+        n = nums.length;
+        BIT = new int[n + 1];
+        for (int i = 0; i < n; i++)
+            init(i, nums[i]);
+    }
+
+    public void init(int i, int val) {
+        i++;
+        while (i <= n) {
+            BIT[i] += val;
+            i += (i & -i);
         }
     }
-    private int lsb(int num){
-        return Integer.lowestOneBit(num);
+
+    void update(int i, int val) {
+        int diff = val - nums[i];
+        nums[i] = val;
+        init(i, diff);
     }
 
-    public long prefixSum(int i){
-        long sum = 0L;
-        while(i > 0){
-            sum += tree[i];
-            i -= lsb(i);
+    public int getSum(int i) {
+        int sum = 0;
+        i++;
+        while (i > 0) {
+            sum += BIT[i];
+            i -= (i & -i);
         }
         return sum;
     }
 
-    public long rangeQuery(int left, int right){
-
-        return left < right ? prefixSum(right) - prefixSum(left - 1) : -1;
-    }
-
-    public void add(int i, long v){
-        while(i < N){
-            tree[i] += v;
-            i = i + lsb(i);
-        }
+    public int sumRange(int i, int j) {
+        return getSum(j) - getSum(i - 1);
     }
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
